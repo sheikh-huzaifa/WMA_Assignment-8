65c39ea7323d93c240bfb5e242c06d3eceb57620
Huzaifa Hameed <123205960+sheikh-huzaifa@users.noreply.github.com>
2023-02-14 22:52:37 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,77 @@
+// 1 task
+let name="Umair Azmat";
+let admin=name;
+alert(admin);
+
+
+// 2 task
+let ourPlanet="Earth";
+
+// 3 task
+let currentVisitor;
+
+// 4 task
+let favDrink = "Coke";
+
+// 5 task
+let favNumber = 9;
+
+// 6 task
+function myFavs(){
+    console.log(favDrink);
+    console.log(favNumber);
+}
+
+myFavs();
+
+// 7task
+let person = {
+    name: "Umair Azmat" ,
+    age: 20
+};
+
+// 8 task
+let a =10;
+let b = 30;
+let sum = a + b;
+console.log(sum);
+
+// 9 task
+let divide = 30 / 5 ;
+console.log("Result of 30 divide by 5 is:" + divide);
+
+// 10 task
+
+let add = 10 + 20;
+let divide1 = add / 5;
+console.log(divide1);
+
+// 11 task
+let f = 50;
+let c = ((f - 32) * 5) / 9;
+console.log("divide two number:  " + c);
+
+// 12 task
+let num1 = parseInt(prompt("type a first number to add"));
+let num2 = parseInt(prompt("type a second number to add in 1st value"));
+console.log(`${num1} + ${num2} = ${num1 + num2}`);
+
+// 13 task
+let diff1 = parseInt(prompt("type a  first number for diff"));
+let diff2 = parseInt(prompt("type a second number from value 1 diff"));
+console.log(`${diff1} - ${diff2} = ${diff1 - diff2}`);
+
+// 14 task
+let product1 = parseInt(prompt("type a first number for product"));
+let product2 = parseInt(prompt("type a second number from value 1 product"));
+console.log(`${product1} * ${product2} = ${product1 * product2}`);
+
+// 15 task
+let quotient1 = parseInt(prompt("type first  number"));
+let quotient2 = parseInt(prompt("type a second number to a quotient from 1"));
+console.log(`${quotient1} / ${quotient2} = ${quotient1 / quotient2}`);
+
+//16 task
+let remainder1 = parseInt(prompt("type a first number"));
+let remainder2 = parseInt(prompt("type a second number"));
+console.log(`${remainder1} % ${remainder2} = ${remainder1 % remainder2}`);
